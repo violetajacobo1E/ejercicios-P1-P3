285cc4bfad5896a458229b5f5c4c688f4f5c0842
violetajacobo1E <113270189+violetajacobo1E@users.noreply.github.com>
2022-09-12 21:02:14 -0600

    Create Ejecicio2 P2.java

---
+++ Ejecicio2 P2.java
@@ -0,0 +1,12 @@
+package ejercicio4p4;
+public class Ejercicio4p4 {
+    public static void main(String[] args) {
+        for (int i=1; i<=10;i++){
+            System.out.println("********* Tabla del "+i+" *************");
+            for (int j=1; j<=10;j++){
+                System.out.println(i+" multiplicado por "+j+" es igual a " +i*j);    
+            }
+        }
+    }
+
+}
