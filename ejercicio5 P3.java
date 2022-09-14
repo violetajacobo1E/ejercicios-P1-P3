45a8d754c05da2da259b88a826692c793affdf69
violetajacobo1E <113270189+violetajacobo1E@users.noreply.github.com>
2022-09-13 20:13:22 -0600

    Create ejercicio5 P3.java

---
+++ ejercicio5 P3.java
@@ -0,0 +1,16 @@
+package ejercicio5;
+import java.util.Scanner;
+public class Ejercicio5{
+    public static void main(String[] args) {
+           Scanner keyboard = new Scanner(System.in);
+           int password;
+           do {
+           System.out.println("Introduzca su contraseña numerica:");
+           password = keyboard.nextInt();
+           if (password != 1234)
+               System.out.println("la contraseña no es valida.");
+            }
+           while (password != 1234);
+    }
+    
+}
