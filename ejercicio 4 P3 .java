918ea8c8c630a66de899b5a6ee513fa748b4af97
violetajacobo1E <113270189+violetajacobo1E@users.noreply.github.com>
2022-09-13 20:05:08 -0600

    Create ejercicio 4 P3 .java

---
+++ ejercicio 4 P3 .java
@@ -0,0 +1,37 @@
+package ejercicio2p3;
+import java.util.Scanner;
+public class Ejercicio2p3 {
+    public static void main(String[] args) {
+        Scanner objeto = new Scanner (System.in);
+        int valor, conteo = 0;
+        double suma = 0;
+        boolean seguir = false;
+         while (!(seguir))
+        {
+            System.out.println("Ingresar un valor entero, el proceso finalizara cunando imprese un multiplo de 8" );
+            valor=objeto.nextInt();
+            
+            if((valor%8)==0)
+            {
+                seguir=true;
+            } 
+            else
+            {
+            conteo++;
+            suma+=valor;
+            }    
+        } 
+        
+        if(conteo!=0)
+        {
+            System.out.println("La cantidad de valores ingresados es"+conteo);
+            System.out.println("La suma de todos los valores es"+suma);
+        }    
+        else
+            {
+                System.out.println("chao, lea indicaciones :3");
+            }
+    }
+    
+    
+}
